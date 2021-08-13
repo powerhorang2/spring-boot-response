ac129f1c05a68c1379c488b1930abbd41cd0464b
powerhorang2 <tjrdbqls3@naver.com>
2021-08-13 16:28:37 +0900

    Response 내려주기 및 모범사례 / ResponseEntity 로 status 201 Create 로 수정해서 Response값 내려주기 추가

--- response/src/main/java/com/example/response/controller/ApiController.java
+++ response/src/main/java/com/example/response/controller/ApiController.java
@@ -1,7 +1,10 @@
 package com.example.response.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +27,12 @@ public class ApiController {
 	@PostMapping("/json")
 	public User json(@RequestBody User user) {
 		return user;
+	}
+	
+	// ResponseEntity
+	@PutMapping("/put")
+	public ResponseEntity<User> put(@RequestBody User user) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(user);
 	}
 	
 }
