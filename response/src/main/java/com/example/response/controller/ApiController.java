3ebc3ad3e7f49f7c761813806269cea91eba56c0
powerhorang2 <tjrdbqls3@naver.com>
2021-08-13 16:20:35 +0900

    Response 내려주기 및 모범사례 / Json 받고 Json으로 보내주기 및 JsonNaming 어노테이션 이용 추가

--- response/src/main/java/com/example/response/controller/ApiController.java
+++ response/src/main/java/com/example/response/controller/ApiController.java
@@ -1,9 +1,13 @@
 package com.example.response.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.response.dto.User;
 
 @RestController
 @RequestMapping("/api")
@@ -13,6 +17,13 @@ public class ApiController {
 	@GetMapping("/text")
 	public String text(@RequestParam String account) {
 		return account;
+	}
+	
+	// JSON
+	// request -> object mapper -> object -> method -> object -> object mapper -> json -> response
+	@PostMapping("/json")
+	public User json(@RequestBody User user) {
+		return user;
 	}
 	
 }
---
+++ response/src/main/java/com/example/response/dto/User.java
@@ -0,0 +1,75 @@
+package com.example.response.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class User {
+
+	private String name;
+	private int age;
+	private String phoneNumber;
+	private String address;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + ", phoneNumber=" + phoneNumber + ", address=" + address + "]";
+	}
+
+}
